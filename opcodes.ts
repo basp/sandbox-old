8e4d645d9c2607f4cb0f862cc8f4ca5d8068d815
Bas Pennings <baspster@gmail.com>
2015-10-08 00:30:08 +0200

    Should be enough formatting for now

--- opcodes.ts
+++ opcodes.ts
@@ -1,22 +1,22 @@
 const enum Op {
-	IMM,
-	IF,
-	EQ, 
-	NEQ,
-	MAKE_EMPTY_LIST,
-	MAKE_SINGLETON_LIST,
-	LIST_APPEND,
-	LIST_CONCAT,
-	JUMP,
-	CALL_BI,
-	CALL_VERB,
-	FORK,
-	FORK_ID,
-	RETURN,
-	RETURN0,
-	EXTENDED,
-	TINY_INT_START,
-	LAST_OP_CODE = 255
+    IMM,
+    IF,
+    EQ, 
+    NEQ,
+    MAKE_EMPTY_LIST,
+    MAKE_SINGLETON_LIST,
+    LIST_APPEND,
+    LIST_CONCAT,
+    JUMP,
+    CALL_BI,
+    CALL_VERB,
+    FORK,
+    FORK_ID,
+    RETURN,
+    RETURN0,
+    EXTENDED,
+    TINY_INT_START,
+    LAST_OP_CODE = 255
 }
 
 const TINY_INT_LOW = -10;
@@ -41,25 +41,25 @@ function inOptimNumRange(i: number): boolean {
 */
  
 function isTinyIntOpCode(o: Op): boolean {
-	return o >= Op.TINY_INT_START;
+    return o >= Op.TINY_INT_START;
 }
 
 function opCodeToTinyInt(o: Op): number {
-	return o - Op.TINY_INT_START + TINY_INT_LOW;
+    return o - Op.TINY_INT_START + TINY_INT_LOW;
 }
 
 function tinyIntToOpCode(i: number): Op {
-	return Op.TINY_INT_START + i - TINY_INT_LOW;
+    return Op.TINY_INT_START + i - TINY_INT_LOW;
 }
 
 function inTinyIntRange(i: number): boolean {
-	return i >= TINY_INT_LOW && i <= TINY_INT_HI;
+    return i >= TINY_INT_LOW && i <= TINY_INT_HI;
 }
  
 export {
-	Op,
-	tinyIntToOpCode,
-	opCodeToTinyInt,
-	inTinyIntRange,
-	isTinyIntOpCode		
+    Op,
+    tinyIntToOpCode,
+    opCodeToTinyInt,
+    inTinyIntRange,
+    isTinyIntOpCode		
 }
--- utils.ts
+++ utils.ts
@@ -1,15 +1,15 @@
 /// <reference path="typings/tsd.d.ts" />
 
 function readInt32(b: Buffer): number {
-	var n = 0;
-	if (b.length > 3) {
-		n += b.readInt32BE(0);
-	} else if (b.length > 1) {
-		n += b.readInt16BE(0);
-	} else if (b.length > 0) {
-		n += b.readInt8(0);
-	}
-	return n;
+    var n = 0;
+    if (b.length > 3) {
+        n += b.readInt32BE(0);
+    } else if (b.length > 1) {
+        n += b.readInt16BE(0);
+    } else if (b.length > 0) {
+        n += b.readInt8(0);
+    }
+    return n;
 }
 
 export { readInt32 }
