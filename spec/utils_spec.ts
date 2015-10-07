36645660b6b934ad0fef65ce34fa35e441e2c789
Bas Pennings <baspster@gmail.com>
2015-10-08 00:28:08 +0200

    Ugh formatting

--- spec/utils_spec.ts
+++ spec/utils_spec.ts
@@ -3,33 +3,33 @@
 import * as utils from '../utils';
 
 describe('readInt32', () => {
-	it('reads an int32 from a 1 byte buffer', () => {
-		const v = -10;
-		
-		var b = new Buffer(1);
-		b.writeInt8(v, 0);
-		
-		var i = utils.readInt32(b);
-		expect(i).toBe(v);
-	});
-	
-	it('reads an int32 from a 2 byte buffer', () => {
-		const v = -4000;
-		
-		var b = new Buffer(2);
-		b.writeInt16BE(v, 0);
-		
-		var i = utils.readInt32(b);
-		expect(i).toBe(v); 
-	});
-	
-	it('reads an int32 from a 4 byte buffer', () => {
-		const v = -5 * 1000 * 1000;
-		
-		var b = new Buffer(4);
-		b.writeInt32BE(v, 0);
-		
-		var i = utils.readInt32(b);
-		expect(i).toBe(v);		
-	});
+    it('reads an int32 from a 1 byte buffer', () => {
+        const v = -10;
+        
+        var b = new Buffer(1);
+        b.writeInt8(v, 0);
+        
+        var i = utils.readInt32(b);
+        expect(i).toBe(v);
+    });
+    
+    it('reads an int32 from a 2 byte buffer', () => {
+        const v = -4000;
+        
+        var b = new Buffer(2);
+        b.writeInt16BE(v, 0);
+        
+        var i = utils.readInt32(b);
+        expect(i).toBe(v); 
+    });
+    
+    it('reads an int32 from a 4 byte buffer', () => {
+        const v = -5 * 1000 * 1000;
+        
+        var b = new Buffer(4);
+        b.writeInt32BE(v, 0);
+        
+        var i = utils.readInt32(b);
+        expect(i).toBe(v);		
+    });
 });
